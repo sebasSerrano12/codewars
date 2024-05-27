c645ea3d8b10f4c62b993bc9c78fcfedb9dc1765
Sebastian Serrano <se.serrano12@gmail.com>
2024-05-27 04:55:19 -0400

    Create boolConvert.js

---
+++ boolConvert.js
@@ -0,0 +1,17 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+// function boolToWord( bool ){
+//   if (bool ===true){
+//     return "Yes"
+//   } else{
+//     return "No"
+//   }
+// }
+
+//My Solution
+// b === t; Yes. b === f; No
+function boolToWord(b){
+  return b === true ? "Yes"
+  : "No"
+}
+//function boolToWord(true) => return Yes
+//function boolToWord(false) => return No
