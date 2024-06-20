7080bcc9a40c74c8eb1aad07f167f617a48e0e8c
Sebastian Serrano <se.serrano12@gmail.com>
2024-06-20 07:23:00 -0400

    Create alanPartridgeII.js

---
+++ alanPartridgeII.js
@@ -0,0 +1,11 @@
+//Instructions
+// Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+// Note: Input will either be a positive integer (or a string for untyped languages).
+//My Solution
+function apple(x){
+  if (x * x > 1000){
+    return "It's hotter than the sun!!"
+  }else {
+    return 'Help yourself to a honeycomb Yorkie for the glovebox.'
+  }
+}
